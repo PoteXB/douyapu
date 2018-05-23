84ad7b8dd12800f333296f8fc57ad24338aca243
PoteXB <baishuangchun@sina.com>
2018-05-23 11:41:53 +0800

    加了个console.log(1);

--- moguTQ/local/main.js
+++ moguTQ/local/main.js
@@ -2408,6 +2408,7 @@
         startDelay:1000,
         loop:true,
     });
+    console.log(1);
     //设置项此次关闭
     $("#sj2018kj .closethisone").click(function () {
         $("#sj2018kj").addClass("hidden");
